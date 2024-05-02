ce50005a76f9bc7a14f7027e23d4cf4711e14726
Jordan <127216241+Jordan-Boulton1@users.noreply.github.com>
2024-05-03 00:54:21 +0100

    feat: #1 setup registration url

--- grooming_service/models.py
+++ grooming_service/models.py
@@ -1,8 +1,6 @@
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db import models
-from django.forms import ValidationError
 from django.utils.translation import gettext_lazy as _
-import re
 from .validators import Validators
 
 
