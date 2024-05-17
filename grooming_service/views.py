4deeb4a2fa134c20d49c90810dc7775f7a2abc81
Jordan <127216241+Jordan-Boulton1@users.noreply.github.com>
2024-05-17 17:34:34 +0100

    fix: #4 fix edit appointment flow

--- grooming_service/views.py
+++ grooming_service/views.py
@@ -76,9 +76,8 @@ def book_appointment(request):
 
             try:
                 check_appointment = Appointment.objects.get(start_date_time=start_date_time, status=1)
-                if check_appointment:
-                    messages.error(request, "The selected appointment is no longer available")
-                    return redirect("appointment")
+                messages.error(request, "The selected appointment is no longer available")
+                return redirect("appointment")
             except Appointment.DoesNotExist:
                 appointment = Appointment(user=request.user,
                                           pet=pet,
@@ -100,71 +99,35 @@ def book_appointment(request):
 
 @login_required(login_url='login')
 def manage_appointments(request):
-    form = PartialAppointmentForm(request.POST or None)
+    form = AppointmentForm(request.POST or None)
 
     if request.method == "POST":
         appointment_id = request.POST.get("appointment_id")
-        start_date = request.POST.get("start_date")
+        start_date_time = request.POST.get("start_date_time")
+        start_date_time = datetime.strptime(start_date_time, "%d-%m-%Y %H:%M").strftime('%Y-%m-%d %H:%M')
         description = form.data["description"]
         pet = form.data["pet"]
         service = form.data["service"]
 
-
         try:
-            start_time_obj = datetime.strptime(start_time, "%H:%M:%S").time()
-            start_date_obj = datetime.strptime(start_date, "%d-%m-%Y").date()
-            start_date_time = datetime.combine(start_date_obj, start_time_obj)
             appointment = Appointment.objects.get(id=appointment_id)
-            if appointment.start_date_time != start_date_time:
-                selected_appointment = Appointment.objects.get(status=0, start_date_time=start_date_time)
-                selected_appointment.status = 1
-                selected_appointment.save()
-                try:
-                    previous_appointment = Appointment.objects.get(
-                        status=1,
-                        start_date_time=appointment.start_date_time,
-                        pet_id=None, service_id=None)
-                    previous_appointment.status = 0
-                    previous_appointment.save()
-                except Appointment.DoesNotExist:
-                    appointment.start_date_time = start_date_time
-                    appointment.pet_id = pet
-                    appointment.service_id = service
-                    appointment.description = description
-                    appointment.save()
-            appointment.start_date_time = start_date_time
+            try:
+                if appointment.start_date_time != start_date_time:
+                    check_appointment = Appointment.objects.get(start_date_time=start_date_time, status=1)
+                    messages.error(request, "The selected appointment is no longer available")
+            except Appointment.DoesNotExist:
+                appointment.start_date_time = start_date_time
             appointment.pet_id = pet
             appointment.service_id = service
             appointment.description = description
-
             appointment.save()
-
+            messages.success(request, "Your appointment has been successfully updated")
             return redirect("profile")
         except Appointment.DoesNotExist:
-            messages.error(request, "The selected appointment is no longer available")
+            messages.error(request, "The requested appointment does not exist")
 
     appointments = Appointment.objects.filter(user=request.user).order_by('start_date_time')
     return render(request, "grooming_service/profile.html", {'form': form, 'appointments': appointments})
-
-
-@login_required(login_url='login')
-def get_available_times(request, selected_date):
-    date = datetime.strptime(selected_date, "%d-%m-%Y").date()
-
-    available_times = []
-    for dt in Appointment.objects.filter(status=0, start_date_time__date=date).values_list("start_date_time",
-                                                                                           flat=True):
-        available_times.append(dt.time().isoformat())
-    return JsonResponse(available_times, safe=False)
-
-
-@login_required(login_url='login')
-def get_available_dates(request):
-    available_dates = []
-    for dt in Appointment.objects.filter(status=0, start_date_time__gte=timezone.now()).values_list("start_date_time",
-                                                                                                    flat=True):
-        available_dates.append(dt.date().isoformat())
-    return JsonResponse(available_dates, safe=False)
 
 
 @login_required(login_url='login')
@@ -174,8 +137,7 @@ def get_appointment_by_id(request, appointment_id):
             appointment = Appointment.objects.get(id=appointment_id)
             appointment_data = {
                 "id": appointment.id,
-                "start_date": appointment.start_date_time.date().strftime('%d-%m-%Y'),
-                "start_time": appointment.start_date_time.time(),
+                "start_date_time": appointment.start_date_time,
                 "description": appointment.description,
                 "pet": {
                     "name": appointment.pet.pet_name,
