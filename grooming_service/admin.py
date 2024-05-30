9ebe8e51e481a2c75b5d12b9413d506f96a765a8
Jordan <127216241+Jordan-Boulton1@users.noreply.github.com>
2024-05-30 21:19:34 +0100

    feat: add comments to admin.py

--- grooming_service/admin.py
+++ grooming_service/admin.py
@@ -4,13 +4,20 @@ from .models import *
 
 # Register your models here.
 
+# Register the Service model with the admin interface
 @admin.register(Service)
 class PostAdmin(SummernoteModelAdmin):
-
+    # Enable Summernote editor for 'description' and 'short_description' fields
     summernote_fields = ('description', 'short_description')
 
+
+# Define an admin class for Appointment model to exclude certain fields
 class AppointmentAdmin(admin.ModelAdmin):
     exclude = ['user', 'pet', 'service']
 
+
+# Register the User model with the admin interface
 admin.site.register(User)
+
+# Register the Appointment model with the custom AppointmentAdmin class
 admin.site.register(Appointment, AppointmentAdmin)
