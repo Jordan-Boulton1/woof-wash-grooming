8777726deb4ed21ecec8912f16a9bd7cf705f9c5
Jordan <127216241+Jordan-Boulton1@users.noreply.github.com>
2024-05-30 23:02:47 +0100

    feat: add profile.js comments

--- static/js/profile.js
+++ static/js/profile.js
@@ -1,5 +1,6 @@
-// Prevents the POST from running again on refresh or back button.
+// Check if the browser supports the replaceState method
 if (window.history.replaceState) {
+    // Replace the current history state with a new one to prevent form resubmission
   window.history.replaceState(null, null, window.location.href);
 }
 
@@ -24,14 +25,19 @@ document.addEventListener("DOMContentLoaded", function () {
   const editPetButtons = document.querySelectorAll('.editPetBtn');
 
   // Handling tab triggers
-  const triggerTabList = document.querySelectorAll('#v-tabs-tab button')
+  const triggerTabList = document.querySelectorAll('#v-tabs-tab button');
+
+  // Loop through each tab button
   triggerTabList.forEach(triggerEl => {
-    const tabTrigger = new bootstrap.Tab(triggerEl)
+    const tabTrigger = new bootstrap.Tab(triggerEl);
+
+    // Add a click event listener to each tab button
     triggerEl.addEventListener('click', event => {
-      event.preventDefault()
-      tabTrigger.show()
+      event.preventDefault();
+      tabTrigger.show();
     });
   });
+
 
   // Handle cancel appointment
   handleConfirmationModal(
@@ -39,45 +45,57 @@ document.addEventListener("DOMContentLoaded", function () {
     "cancelAppointmentForm",
     cancelButtons,
     confirmCancelButton,
-    cancelAppointmentIdField);
+    cancelAppointmentIdField
+  );
 
   // Handle edit appointment
   handleFormSubmit(
     "saveChangesButton",
     "editAppointmentForm",
-    "editAppointmentFormErrors")
+    "editAppointmentFormErrors"
+  );
 
   // Handle edit appointment modal
   editButtons.forEach(function (btn) {
+    // Get the modal element for editing appointments
     const appointmentModal = document.getElementById("editAppointmentModal");
+    // Add a click event listener to each edit button
     btn.addEventListener('click', function () {
+      // Get the appointment ID from the buttons attribute
       const appointmentId = btn.getAttribute('id');
 
+      // Initialize Flatpickr date-time picker
       renderFlatPickr();
+      // Render the calendar icon for the date field
       renderCalendarIcon(dateFieldIcon, dateField);
+      // Clear the appointment ID field
       appointmentIdField.innerHTML = "";
+      // Fetch appointment data from the API
       fetch(`/api/appointment/${appointmentId}/`)
         .then((response) => response.json())
         .then((data) => {
-          const appointment = data.appointment
+          const appointment = data.appointment;
 
-          setDefaultOption(dateField, appointment.start_date_time)
+          // Set default options for the form fields with fetched data
+          setDefaultOption(dateField, appointment.start_date_time);
           setDefaultOption(description, appointment.description);
           setDefaultSelectOption(petField, appointment.pet.id);
           setDefaultSelectOption(serviceField, appointment.service.id);
+          // Set the appointment ID in the hidden field
           appointmentIdField.value = appointmentId;
-        })
+        });
 
+      // Show the modal for editing appointments
       const modal = new bootstrap.Modal(appointmentModal);
       modal.show();
-
-
     });
   });
 
   // Show add pet modal
   addPetButton.addEventListener('click', function () {
+    // Get the modal element for adding a pet
     const modal = new bootstrap.Modal(addPetModal);
+    // Show the modal for adding a pet
     modal.show();
   });
 
@@ -85,19 +103,26 @@ document.addEventListener("DOMContentLoaded", function () {
   handleFormSubmit(
     "addPetSubmitButton",
     "addPetForm",
-    "petFormErrors")
+    "petFormErrors"
+  );
 
   // Handle edit pet modal
   editPetButtons.forEach(function (btn) {
+    // Get the modal element for editing pets
     const editPetModal = document.getElementById("editPetModal");
+    // Add a click event listener to each edit pet button
     btn.addEventListener('click', function () {
+      // Get the pet ID from the button's attribute
       const petId = btn.getAttribute('id');
 
+      // Clear the pet ID field
       petIdField.innerHTML = "";
+      // Fetch pet data from the API
       fetch(`/api/pet/${petId}/`)
         .then((response) => response.json())
         .then((data) => {
           const pet = data.pet
+          // Set default options for the form fields with the fetched pet data
           petIdField.value = petId;
           const petNameField = document.getElementById("edit_pet_name");
           const petBreedField = document.getElementById("edit_pet_breed");
@@ -107,28 +132,29 @@ document.addEventListener("DOMContentLoaded", function () {
           setDefaultOption(petBreedField, pet.breed);
           setDefaultOption(petAgeField, pet.age);
           setDefaultOption(petMedicalNotesField, pet.medical_notes);
-        })
+        });
 
+      // Show the modal for editing pets
       const modal = new bootstrap.Modal(editPetModal);
       modal.show();
-
-
     });
   });
 
-  // Handle edit pet
+  // Handle edit pet form submission
     handleFormSubmit(
     "editPetButton",
     "editPetForm",
-    "editPetFormErrors");
+    "editPetFormErrors"
+    );
 
   // Handle delete pet
   handleConfirmationModal(
       "confirmDeletePetModal",
       "deletePetForm",
       deletePetButtons,
-    confirmDeletePetButton,
-      deletePetIdField);
+      confirmDeletePetButton,
+      deletePetIdField
+  );
 });
 
 // Render Flatpickr date-time picker
@@ -148,18 +174,40 @@ function renderFlatPickr() {
     "maxTime": "17:00",
   });
 }
-// Render calendar icon
+
+/**
+ * Render calendar icon to trigger opening of the Flatpickr date-time picker.
+ *
+ * @param icon - The calendar icon element.
+ * @param dateField - The input field associated with the Flatpickr date-time picker.
+ */
 function renderCalendarIcon(icon, dateField) {
+  // Add a click event listener to the calendar icon
   icon.addEventListener("click", function (event) {
     event.preventDefault();
+
+    // Open the Flatpickr date-time picker
     dateField._flatpickr.open()
   });
 }
-// Set default option for field
+
+/**
+ * Set the default value for a given field.
+ *
+ * @param field - The field element.
+ * @param value - The default value to be set.
+ */
 function setDefaultOption(field, value) {
+  // Set the value of the field to the provided value
   field.value = value;
 }
-// Set default select option for field
+
+/**
+ * Set the default selected option for a given select field based on the provided ID.
+ *
+ * @param field - The select field element.
+ * @param id - The ID of the option to be selected.
+ */
 function setDefaultSelectOption(field, id) {
   for (let i = 0; i < field.options.length; i++) {
     let option = field.options[i];
@@ -169,46 +217,80 @@ function setDefaultSelectOption(field, id) {
     }
   }
 }
+
+/**
+ * Convert the format of a date-time string from "d-m-Y H:i" to "Y-m-d H:i".
+ *
+ * @param dateTimeStr - The date-time string in "d-m-Y H:i" format.
+ * @returns The date-time string converted to "Y-m-d H:i" format.
+ */
 function convertDateTimeFormat(dateTimeStr) {
-  // Assuming the incoming date format is "d-m-Y H:i"
-  // Convert it to "Y-m-d H:i"
   const [datePart, timePart] = dateTimeStr.split(' ');
   const [day, month, year] = datePart.split('-');
   return `${year}-${month}-${day} ${timePart}`;
 }
+
+/**
+ * Handle the confirmation modal for various actions triggered by specific buttons.
+ *
+ * @param modal - The ID of the confirmation modal element.
+ * @param confirmationForm - The ID of the form associated with the confirmation action.
+ * @param triggerButtons - The NodeList of buttons that trigger the confirmation modal.
+ * @param confirmationButton - The confirmation button within the modal.
+ * @param idField - The hidden input field storing the ID associated with the action.
+ */
 function handleConfirmationModal(modal, confirmationForm, triggerButtons, confirmationButton, idField) {
+  // Iterate over each trigger button
   triggerButtons.forEach(function (btn) {
+    // Get the confirmation modal
     const confirmationModal = document.getElementById(modal);
+    // Add click event listener to the trigger button
     btn.addEventListener('click', function (event) {
       event.preventDefault();
       const id = btn.getAttribute('id');
 
+      // Show the confirmation modal
       const modal = new bootstrap.Modal(confirmationModal);
       modal.show();
 
+      // Add click event listener to the confirmation button within the modal
       confirmationButton.addEventListener("click", function () {
         idField.value = id;
         let form = document.getElementById(confirmationForm);
+        // Update the form action URL to include the ID
         form.action = form.action.replace('/0/', '/' + id + '/');
         form.submit();
       });
     });
   });
 }
+
+/**
+ * Handle form submission asynchronously.
+ *
+ * @param submitButton - The ID of the submit button.
+ * @param submitForm - The ID of the form to be submitted.
+ * @param formErrorContainer - The ID of the container to display form errors.
+ */
 function handleFormSubmit(submitButton, submitForm, formErrorContainer) {
+  // Add click event listener to the submit button
   document.getElementById(submitButton).addEventListener('click', function (event) {
     event.preventDefault();
     const form = document.getElementById(submitForm);
     const formData = new FormData(form);
     const formType = formData.get("form_type");
 
+    // If the form type is edit_appointment_form, convert the date-time format
     if (formType === 'edit_appointment_form') {
       const startDateTime = formData.get('start_date_time');
       const convertedDateTime = convertDateTimeFormat(startDateTime);
       formData.set('start_date_time', convertedDateTime);
     }
 
+    // Clear the form error container
     document.getElementById(formErrorContainer).innerHTML = '';
+
+    // Send a fetch request to the form action URL with form data
     fetch(form.action, {
       method: 'POST',
       body: formData,
