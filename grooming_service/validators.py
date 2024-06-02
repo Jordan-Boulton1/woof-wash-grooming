0f1ac382c07ae02f59c1ccf8d69ffe6f281897ef
Jordan <127216241+Jordan-Boulton1@users.noreply.github.com>
2024-06-02 21:49:46 +0100

    refactor: improve validators

--- grooming_service/validators.py
+++ grooming_service/validators.py
@@ -4,33 +4,93 @@ from django.utils.translation import gettext_lazy as _
 
 
 class Validators:
+    """
+    Utility class containing validation methods.
+    """
 
     def validate_string_input(value):
         """
-        This validator checks if a given string contains only letters (a-z, A-Z).
-        If the string contains anything other than letters, it raises a ValidationError.
-        """
-        # You can use regular expressions to validate the input.
+       Validation method that checks if a given string contains only letters (a-z, A-Z).
+       If the string contains anything other than letters, it raises a ValidationError.
+
+       Args:
+           value (str): The string value to validate.
+
+       Raises:
+           ValidationError: If the string contains non-letter characters.
+       """
+        # Regular expressions to check if the input contains only letters.
         if not re.match("^[a-zA-Z]+$", value):
             raise ValidationError(_("This field must contain only letters."))
 
     def validate_phone_number(value):
         """
-        This validator checks if a given string contains only digits (0-9).
-        If the string contains anything other than digits, it raises a ValidationError.
-        """
-        # Validate phone number (simple example: ensuring it contains only digits)
+         Validation method that checks if a given string contains only digits (0-9).
+          If the string contains anything other than digits, it raises a ValidationError.
+
+          Args:
+              value (str): The phone number string to validate.
+
+          Raises:
+              ValidationError: If the phone number contains non-digit characters.
+          """
+        # Regular expressions to check if the input contains only letters.
         if not re.match(r"^\d+$", value):
             raise ValidationError("Phone number can contain only digits.")
 
-    def append_error_messages_when_field_is_empty(formField, errorMessage, errorMessages):
-        if not formField or formField is None:
-            errorMessages.append(errorMessage)
-        return errorMessages
+    @staticmethod
+    def append_error_messages_when_field_is_empty(field, message, errors):
+        """
+           Validation method that checks if a field is empty and appends an error message if it is.
 
-    def append_error_messages_when_field_does_not_match_regex(formField, regex, errorMessage, errorMessages):
-        if formField and not re.match(regex, formField):
-            errorMessages.append(errorMessage)
-        return errorMessages
+           Args:
+               field: The field value to check.
+               message (str): The error message to append if the field is empty.
+               errors (list): The list of current errors.
 
+           Returns:
+               list: The updated list of errors.
+        """
+        if not field:
+            errors.append(message)
+        return errors
 
+    @staticmethod
+    def append_error_messages_when_field_does_not_match_regex(field, regex, message, errors):
+        """
+        Validation method that checks if a field's value matches a given regex pattern and appends an error message
+        if it does not.
+
+           Args:
+               field: The field value to check.
+               regex (str): The regex pattern to match against.
+               message (str): The error message to append if the field does not match the regex.
+               errors (list): The list of current errors.
+
+           Returns:
+               list: The updated list of errors.
+        """
+        if field and not re.match(regex, field):
+            errors.append(message)
+        return errors
+
+    @staticmethod
+    def append_uniqueness_error(field, model, field_name, message, errors):
+        """
+        Validation method that checks if a field's value is unique within a given model and appends an error message
+        if it is not.
+
+           Args:
+               field: The field value to check.
+               model: The Django model to check against.
+               field_name (str): The name of the field in the model to check for uniqueness.
+               message (str): The error message to append if the field value is not unique.
+               errors (list): The list of current errors.
+
+           Returns:
+               list: The updated list of errors.
+        """
+        if field:
+            if model.objects.filter(**{field_name: field}).exists():
+                errors.append(message)
+        return errors
