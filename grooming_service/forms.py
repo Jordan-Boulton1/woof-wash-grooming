0f6c74535db7e8426950513806fb44e63b9f0771
Jordan <127216241+Jordan-Boulton1@users.noreply.github.com>
2024-06-03 21:34:36 +0100

    fix: indentation error

--- grooming_service/forms.py
+++ grooming_service/forms.py
@@ -194,12 +194,12 @@ class EditUserForm(BaseForm):
 # Form for user login
 class LoginForm(forms.Form):
     email = forms.EmailField(
-        widget=forms.EmailInput(attrs={'class': 'form-control', 'required':
-            'true'})
+        widget=forms.EmailInput(attrs={'class': 'form-control',
+                                       'required': 'true'})
     )
     password = forms.CharField(
-        widget=forms.PasswordInput(attrs={'class': 'form-control', 'required':
-            'true'})
+        widget=forms.PasswordInput(attrs={'class': 'form-control',
+                                          'required': 'true'})
     )
 
 
